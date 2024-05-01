0445bcbacc9d0bb287a094266931d46c1aa17296
hamdan1920 <168641774+hamdan1920@users.noreply.github.com>
2024-05-01 20:51:27 +0700

    server.py

--- server.py
+++ server.py
@@ -1,25 +1,18 @@
 import socket
-import sys
 
-s = socket.socket()
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('0.0.0.0', 10013))
-s.listen(0)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address ('localhost', 12345)
+server_socket.bind(server_address)
+server_socket.listen(5)
+
+print("Server ", server_address)
 
 while True:
-
-    client, addr = s.accept()
-
-    while True:
-        data = client.recv(32)
-
-        if len(data)  ==0:
-            break
-
-        else:
-            sys.stdout.write(data.decode())
-            sys.stdout.flush()
-
-
-
-    client.close()
+    client_socket, client_address = server_socket.accept()
+    try:
+        print("Terhubung > ", client_address)
+        data = client_socket.recv(1824)
+        print("Menerima:", data.decode())
+        client_socket.sendall(b"Terima kasihl")
+    finally:
+        client_socket.close()
